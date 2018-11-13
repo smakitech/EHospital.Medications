d13a73f9d2327646fd21905578a9edecd1fe9b88
smoukiDev <smakfactory@gmail.com>
2018-11-13 23:40:15 +0200

    Connect Swagger and StyleCop to API project.

--- DP148.eHealth.Medications/DP148.eHealth.API.Medications/Startup.cs
+++ DP148.eHealth.Medications/DP148.eHealth.API.Medications/Startup.cs
@@ -10,11 +10,17 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DP148.eHealth.API.Medications
 {
     public class Startup
     {
+        ///* Swagger constants
+        private const string VERSION = "v.1.0";
+        private const string API_NAME = "eHealth.Medications.API";
+        //*/
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +32,23 @@ namespace DP148.eHealth.API.Medications
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            ///* Swagger Setting
+            Info info = new Info
+            {
+                Version = VERSION,
+                Title = API_NAME,
+                Description = "Microservic contain business logic "
+                            + "to manage medications and prescriptions to patient.",
+                Contact = new Contact()
+                {
+                    Name = "Serhii Maksymchuk",
+                    Email = "smakdealcase@gmail.com",
+                    Url = "https://github.com/smoukiDev/DP148.eHealth.Medications"
+                }
+            };
+            services.AddSwaggerGen(c => { c.SwaggerDoc(VERSION, info); });
+            //*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +65,14 @@ namespace DP148.eHealth.API.Medications
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            ///* Swagger Setting
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/swagger/{VERSION}/swagger.json", API_NAME);
+            });
+            //*/
         }
     }
 }
